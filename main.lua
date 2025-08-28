d4891d436ea0c63a9288fd13e89d72aac8eaf3bd
ethz <135509198+ethz07@users.noreply.github.com>
2025-08-29 02:50:07 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,9 +1,3 @@
-if game.CoreGui.RobloxGui:FindFirstChild("ScreenGui") then
-game.StarterGui:SetCore("SendNotification",{["Title"]="Error",["Text"]="You cannot re-run the cheat!",["Duration"]=4})
-else
-wait (1)
-game.StarterGui:SetCore("SendNotification",{["Title"]="ethz Script",["Text"]="Cheat Loaded.",["Duration"]=4})
-wait (1)
 -- Gui to Lua
 -- Version: 3.2
 
